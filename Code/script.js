d7e4ef30d5a5a5e67d0bf5960f56174acef6aa24
JJThatGuy <jguy624@canon-slade.bolton.sch.uk>
2025-03-03 10:40:29 +0000

    roda

--- Code/script.js
+++ Code/script.js
@@ -32,7 +32,7 @@ function setup() {
    
    
     player = {
-        x: 20,
+        x: 0,
         y: -50,
         z: 2000,
         speed: 30,
@@ -114,8 +114,10 @@ function createWalls() {
     //200px = 1 meter = scale
 
     walls.push({ x: -20000, y: 250, z: 0, width: 80000, height: 5, depth: 40000 }); // Grass stuff
-    Pavement.push({ x: 405, y: 240, z: -120, width: 16000, height: 20, depth: 300 }); // Pavemnt
-    Road.push({ x: 405, y: 240, z: 500, width: 16000, height: 10, depth: 1000 }); // Road
+    Pavement.push({ x: 405, y: 240, z: -120, width: 3200, height: 20, depth: 300 }); // Pavemnt
+    Pavement.push({ x: 405, y: 240, z: 1120, width: 10000, height: 20, depth: 300 }); // Pavemnt
+    Road.push({ x: 405, y: 240, z: 500, width: 10000, height: 10, depth: 1000 }); // Road
+    Road.push({ x: -1700, y: 240, z: -2500, width: 1000, height: 10, depth: 5000 }); // Road
 
     /* Create the walls of the first room
     walls.push({ x: 0, y: 0.3,  z: -250, width: 500, height: 500, depth: 5 }); // Back wall
@@ -142,8 +144,8 @@ function createWalls() {
     Windows.push({ x: -805, y: -1250 ,  z: -280, width: 800, height: 200, depth:10 }); 
     walls.push({ x: -805, y: -1450,  z: -285, width: 800, height: 200, depth:30 }); 
 
-    sidewalls.push({ x: -1205, y: -550 ,  z: -485, width: 10, height: -2000, depth:500 }); 
     sidewalls.push({ x: -405, y: -550 ,  z: -485, width: 10, height: -2000, depth:500 }); 
+    sidewalls.push({ x: -1205, y: -550 ,  z: -485, width: 10, height: -2000, depth:500 }); 
     sidewalls.push({ x: -805, y: -550,  z: -720, width: 800, height: 2000, depth:30 }); 
     sidewalls.push({ x: -805, y: -1545 ,  z: -485, width: 800, height: 10, depth:-485 });     
 
@@ -179,11 +181,27 @@ function createWalls() {
     sidewalls.push({ x: 405, y: -550 ,  z: -485, width: 10, height: -2000, depth:500 }); 
     sidewalls.push({ x: 805, y: -550,  z: -720, width: 800, height: 2000, depth:30 }); 
     sidewalls.push({ x: 805, y: -1545 ,  z: -485, width: 800, height: 10, depth:-485 }); 
+//---------------------------------------------------------------------------------
+walls.push({ x: 1610, y: 150,  z: -285, width: 800, height: 200, depth:30 }); 
+Windows.push({ x: 1610, y: -50 ,  z: -280, width: 800, height: 200, depth:10 }); 
+walls.push({ x: 1610, y: -250,  z: -285, width: 800, height: 200, depth:30 }); 
+Windows.push({ x: 1610, y: -450 ,  z: -280, width: 800, height: 200, depth:10 }); 
+walls.push({ x: 1610, y: -650,  z: -285, width: 800, height: 200, depth:30 }); 
+Windows.push({ x: 1610, y: -850 ,  z: -280, width: 800, height: 200, depth:10 }); 
+walls.push({ x: 1610, y: -1050,  z: -285, width: 800, height: 200, depth:30 }); 
+
+
+
+   
+
+    sidewalls.push({ x: 2010, y: -350 ,  z: -485, width: 10, height: -1605, depth:500 }); 
+    sidewalls.push({ x: 1600, y: -350,  z: -720, width: 800, height: -1605, depth:30 }); 
+    sidewalls.push({ x: 1600, y: -1150 ,  z: -485, width: 800, height: 10, depth:-485 }); 
 
     //-----------------------------------------------------------------------------------
 
-    walls.push({x:1700, y: 150 , z:-500, width: 1000, height: 200, depth:500})
-    Road.push({x:1700, y: 150 , z:-250, width: 150, height: 400, depth:5})
+  //  walls.push({x:1700, y: 150 , z:-500, width: 1000, height: 200, depth:500})
+  //  Road.push({x:1700, y: 150 , z:-250, width: 150, height: 400, depth:5})
 
   
     
@@ -196,6 +214,8 @@ function createWalls() {
 
 
 function handleMovement() {
+    
+    
 
     if (keyIsDown(87)) { // W key
 
@@ -223,8 +243,6 @@ function handleMovement() {
 
       if (keyIsDown(32)) { // space bar (go up)
         player.y -= 30
-       
-
     }
-
+   
 }
