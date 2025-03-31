86aa2805e89826f981282748826290037e6091af
JJThatGuy <jguy624@canon-slade.bolton.sch.uk>
2025-03-31 10:46:36 +0100

    yaaaaa

--- Code/script.js
+++ Code/script.js
@@ -37,7 +37,7 @@ function setup() {
         x: -7500,
         y: -50,
         z: 7500,
-        speed: 15,
+        speed: 25,
         angle:0
         
     
@@ -119,25 +119,11 @@ function createWalls() {
     //200px = 1 meter = scale
 
     walls.push({ x:40000, y: 250, z: 40000, width: 80000, height: 5, depth: 80000 }); // Grass stuff
-   // Pavement.push({ x: 905, y: 240, z: 420, width: 3200, height: 20, depth: 300 }); // Pavemnt
     Pavement.push({ x: 905, y: 240, z: 620, width: 10000, height: 20, depth: 300 }); // Pavemnt
     Road.push({ x: 905, y: 240, z: 1000, width: 10000, height: 10, depth: 1000 }); // Road
     Road.push({ x: -1200, y: 240, z: -2000, width: 1000, height: 10, depth: 5000 }); // Road
 
-    /* Create the walls of the first room
-    walls.push({ x: 0, y: 0.3,  z: -250, width: 500, height: 500, depth: 5 }); // Back wall
-    walls.push({ x: 0, y: -5 , z: 250, width: 500, height: 500, depth: 5 }); // Front wall
-    walls.push({ x: -250, y: 0.3 , z: 0, width: 5, height: 500, depth: 500 }); // Left wall
-    walls.push({ x: 250, y: 0.1 , z: 0, width: 5, height: 500, depth: 500 });  // Right wall
-    //walls.push({ x: 500, y: 0.1, z: 200, width: 500, height: 500, depth: 5 });
-  
-
    
-    // Create the doorway (between the two rooms)
-    walls.push({ x: 0, y: 150, z: 250, width: doorWidth, height: doorHeight, depth: 5 }); // Doorway
-    */
-   
-// x changes by 805 pixels, y no change and z depends on how far back i want
 
     Floor.push({ x: 400, y: 150,  z: Buildz, width: 800, height: 200, depth:30 }); 
     Windows.push({ x: 400, y: -50 ,  z: Buildz, width: 800, height: 200, depth:10 }); 
@@ -182,10 +168,10 @@ function createWalls() {
     Windows.push({ x: 2000, y: -1250 ,  z: Buildz, width: 800, height: 200, depth:10 }); 
     walls.push({ x: 2000, y: -1450,  z: Buildz, width: 800, height: 200, depth:30 }); 
 
-    sidewalls.push({ x: 1205, y: -550 ,  z: 265, width: 10, height: -2000, depth:500 }); 
-    sidewalls.push({ x: 405, y: -550 ,  z: 265, width: 10, height: -2000, depth:500 }); 
-    sidewalls.push({ x: 805, y: -550,  z: backwallz, width: 800, height: 2000, depth:30 }); 
-    sidewalls.push({ x: 805, y: -1545 ,  z: 265, width: 800, height: 10, depth:-485 }); 
+    sidewalls.push({ x: 1600, y: -550 ,  z: 265, width: 10, height: -2000, depth:500 }); 
+    sidewalls.push({ x: 2400, y: -550 ,  z: 265, width: 10, height: -2000, depth:500 }); 
+    sidewalls.push({ x: 2000, y: -550,  z: backwallz, width: 800, height: -2000, depth:30 }); 
+    sidewalls.push({ x: 2000, y: -1545 ,  z: 265, width: 800, height: 10, depth:-485 }); 
 //---------------------------------------------------------------------------------
 walls.push({ x: 2800, y: 150,  z: Buildz, width: 800, height: 200, depth:30 }); 
 Windows.push({ x: 2800, y: -50 ,  z: Buildz, width: 800, height: 200, depth:10 }); 
@@ -199,14 +185,13 @@ walls.push({ x: 2800, y: -1050,  z: Buildz, width: 800, height: 200, depth:30 })
 
    
 
-    sidewalls.push({ x: 2010, y: -350 ,  z: 265, width: 10, height: -1605, depth:500 }); 
-    sidewalls.push({ x: 1600, y: -350,  z: backwallz, width: 800, height: -1605, depth:30 }); 
-    sidewalls.push({ x: 1600, y: -1150 ,  z: 265, width: 800, height: 10, depth:-485 }); 
+    sidewalls.push({ x: 3200, y: -350 ,  z: 265, width: 10, height: -1605, depth:500 }); 
+    sidewalls.push({ x: 2800, y: -350,  z: backwallz, width: 800, height: -1605, depth:30 }); 
+    sidewalls.push({ x: 2800, y: -1150 ,  z: 265, width: 800, height: 10, depth:-485 }); 
 
     //-----------------------------------------------------------------------------------
 
-  //  walls.push({x:1700, y: 150 , z:-500, width: 1000, height: 200, depth:500})
-  //  Road.push({x:1700, y: 150 , z:-250, width: 150, height: 400, depth:5})
+  
 
   
     
